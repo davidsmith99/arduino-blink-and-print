e6b04bb41712189d644d1c356075b2c1ff4ad00f
unknown <e29152@EK139-PC1.SRI.COM>
2018-01-04 23:27:00 -0800

    Update comments

--- ds_blink_print/ds_blink_print.ino
+++ ds_blink_print/ds_blink_print.ino
@@ -1,32 +1,32 @@
 /*
    Blink & Print
-   Turns on an LED on for a time, then off until the end of the cycle period.
-   Use of millis() time function instead of delay() so it does not block other operations
-   Includes print messages to verify communication and times
+   Turns on an LED on for an arbitrary duty cycle by setting an ON duration and a total period.
+   Use millis() time function instead of delay() so it does not block other operations
+   Add print messages to in addition to LED state
+   
    David Smith
-   This code is in the public domain.
+   2016, updated 2017
 */
 
-// Pin 13 has an LED connected on most Arduino boards, including Teensy 3.
-int PIN_LED = LED_BUILTIN;
-
+// global variables
+int PIN_LED = LED_BUILTIN;      // Pin 13 on most Arduino boards, including Teensy 3.x
 unsigned int t_period = 1200;   // period [ms]
 unsigned int t_on =      141;   // ON duration (must be < t_period)
-const int TIMEOUT_MS = 5000;
+const int TIMEOUT_MS = 5000;    // stop waiting for monitor window to open
 
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
   digitalWrite(PIN_LED, LOW);
-  Serial.begin(57600);                    // rate ignored if using USB port comm
+  Serial.begin(57600); // rate ignored if using USB port comm
   while (!Serial && (millis() < TIMEOUT_MS)) {  // timeout lets program continue if the monitor window is never opened
     ; // wait for serial port to connect. Needed for Leonardo & Teensyduino 3
   }
   Serial.println("Serial port established for printing.");
-
   Serial.print("\n PERIOD: "); Serial.println(t_period);
 }
 
+/// Check the millisecond timer; change state of LED and print a msg at intervals
 void loop() {
   int t;
   static int t_prev = 0;
